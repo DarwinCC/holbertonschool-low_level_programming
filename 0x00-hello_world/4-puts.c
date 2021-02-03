f1f0d351fbd75096823c8216b33fcf5d333cae2f
DarwinCC <darwinpkmz@gmail.com>
2021-02-03 21:14:13 +0000

    Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - punto de entrada de un programa
+ * Return: indicar el valor de retorno 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return (0);
+}

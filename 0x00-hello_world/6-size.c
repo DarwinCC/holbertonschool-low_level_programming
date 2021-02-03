d1a23407475eb354c6ba2aa8a610c2a8c4c04cc9
DarwinCC <darwinpkmz@gmail.com>
2021-02-03 21:21:41 +0000

    Actualización

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * Descripción: main - punto de entrada de un programa
- * return: indicar el valor de retorno
+ * main - C imprime el tamaño
+ * Return: indicar el valor de retorno
  */
 int main(void)
 {
-printf("Size of a char: %d bytes(s)\n", sizeof(char));
-printf("Size of an int: %d bytes(s)\n", sizeof(int));
-printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %d bytes(s)\n", sizeof(float));
-return(0);
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
 }

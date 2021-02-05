ad964ed7e7ce4e4d0c9c99a86e9f10ee5c9ba7eb
DarwinCC <darwinpkmz@gmail.com>
2021-02-05 03:41:59 +0000

    Actualización

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - punto de entrada de un programa
+ * Return: indicar el valor de retorno 0
+ */
+int main(void)
+{
+	int i,f;
+
+	i = 0;
+	while (i <= 9)
+	{
+		f = 0;
+		while (f <= 9)
+		{
+			putchar (0 + i);
+			putchar (0 + f);
+			if (i !=9 || f != 9)
+			{
+				putchar (44);
+				putchar (32);
+			}
+			f = f + 1;
+		}
+		i = i + 1;
+	}
+	putchar ('\n');
+	return (0);
+}

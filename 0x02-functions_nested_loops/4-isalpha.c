9a959e7ae09f1168a5d7fc15f4f82d1485cb2ed7
DarwinCC <darwinpkmz@gmail.com>
2021-02-09 19:29:13 +0000

    Update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
  * _isalpha - función principal
+ * @c: caracter
  * Description: busca caracteres en minúscula o mayúscula
  *
  * Return: indicar el valor de retorno
  */
 int _isalpha(int c)
 {
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-        else
-        return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	return (1);
+	else
+	return (0);
 }

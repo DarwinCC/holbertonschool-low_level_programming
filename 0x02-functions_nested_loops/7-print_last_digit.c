05196bfff34f22747d5a2a8b5f87b9dd6d99e8f5
DarwinCC <darwinpkmz@gmail.com>
2021-02-09 19:51:22 +0000

    Update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,21 @@
 /**
  * print_last_digit - función principal
  * Description: imprime el último dígito de un numero
+ * @r: numero
  *
  * Return: indicar el valor de retorno 0
  */
 int print_last_digit(int r)
 {
-if (r%10 < 0)
-{
-_putchar(-r%10)
-}
-else 
-{
-_putchar(r%10)
-}
-return (r%10)
+	int i = r % 10;
+
+	if (i < 0)
+	{
+	_putchar(-i);
+	}
+	else
+	{
+	_putchar(i);
+	}
+return (i);
 }

07ab7156143fbb23055f5251dcb1a086f993e080
DarwinCC <darwinpkmz@gmail.com>
2021-02-09 19:33:25 +0000

    Update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,13 @@
 /**
  * print_sign - función principal
  * Description: muestra el signo de un número
+ * @n: numero
  *
  * Return: indicar el valor de retorno
  */
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 	{
 	_putchar('+');
 	return (1);
@@ -20,6 +21,6 @@ else if (n == 0)
 else
 	{
 	_putchar('-');
-        return (-1);
+	return (-1);
 	}
 }

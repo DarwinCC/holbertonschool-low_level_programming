3406dd96fb4c59047b065845844de967ef31f9fb
DarwinCC <darwinpkmz@gmail.com>
2021-02-09 21:19:12 +0000

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - función principal
+ * @n: variable
+ * @n1: variable
+ * Description: busca caracteres en minúscula o mayúscula
+ *
+ * Return: indicar el valor de retorno
+ */
+int add(int n, int n1)
+{
+	int suma = n + n1;
+
+	return (suma);
+}

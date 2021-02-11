aa966ff64a09ab076f37eef98a4ebb3e84dd96e4
DarwinCC <darwinpkmz@gmail.com>
2021-02-11 23:09:04 +0000

    Fixed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,17 @@
  */
 void print_line(int n)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
-	while (n >= 1)
+		while (n >= 1)
 		{
 		_putchar('_');
 		n--;
 		}
 		_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
 	}
 }

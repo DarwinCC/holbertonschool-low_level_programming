f527b091531f607005663d6708798b52f5186595
DarwinCC <darwinpkmz@gmail.com>
2021-02-11 23:21:56 +0000

    Fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,24 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if ((i % 5 == 0) && (i % 3 == 0))
-	{
-	printf("FizzBuzz ");
-	}
-	else if (i % 5 == 0)
-	{
-	printf("Buzz ");
-	}
-	else if (i % 3 == 0)
-	{
-	printf("Fizz ");
-	}
-		else
+		if ((i % 5 == 0) && (i % 3 == 0))
 		{
-		printf("%d ", i);
+		printf("FizzBuzz ");
 		}
+		else if (i % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+			else
+			{
+			printf("%d ", i);
+			}
+		if (i < 100)
+		printf(" ");
 	}
 printf("\n");
 return (0);

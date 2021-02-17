0e019e7bdbb7a53e49e9ef8670025ad395a33def
DarwinCC <darwinpkmz@gmail.com>
2021-02-17 17:31:52 +0000

    Fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,13 +8,14 @@
  */
 int main(void)
 {
-	unsigned short int length = 35;
+	int length = 35;
 
 	srand((unsigned int) time(0));
 	while (length--)
 	{
-	putchar(rand() % 128 + 33);
+	putchar(rand() % 94 + 33);
 	}
 	printf("\n");
+
 	return (0);
 }

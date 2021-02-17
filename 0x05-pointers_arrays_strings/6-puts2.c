04ce4e46132a3d973ba5fe7cfa463c7c8b3b8434
DarwinCC <darwinpkmz@gmail.com>
2021-02-17 04:01:22 +0000

    Pointers,...

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * puts2 - funcion principal
+ *
+ * @str: puntero
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+{
+		if (i % 2 == 0)
+		{
+		_putchar (str[i]);
+		}
+	}
+	_putchar ('\n');
+}

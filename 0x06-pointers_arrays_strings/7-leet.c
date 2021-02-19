1286b1c4800731f26e80febecaa9fe4578e840af
DarwinCC <darwinpkmz@gmail.com>
2021-02-19 18:00:43 +0000

    Pointers,..

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,9 +9,17 @@ char *leet(char *string)
 {
 	char letras[] = "aAeEoOtTlL";
 	char numero[] = "4433007711";
-	int i;
+	int i, j; /* i=recore el string y j recorre nuestro array */
 
-	for(i = 0; string[i] ='\0'; i++)
+	for (i = 0; string[i] != '\0'; i++) /* navega dentro de nuestro string */
 	{
-	
+		for (j = 0; letras[j] != '\0'; j++) /* navega dentro de letras */
+		{
+			if (string[i] == letras[j]) /* compara 'string' con 'letras' */
+			{
+			string[i] = numero[j];
+			}
+		}
 	}
+return(string);
+}

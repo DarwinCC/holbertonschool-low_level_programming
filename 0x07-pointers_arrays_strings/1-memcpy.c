eff6f682226926d56202a246e7d3e10ad4cf3272
DarwinCC <darwinpkmz@gmail.com>
2021-02-22 20:45:33 +0000

    More pointers..

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memcpy - funcion principal
+ *
+ * @dest: string destino
+ * @src: string de origen
+ * @n: numeros de bytes a llenar
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+        {
+                dest[i] = src[i];
+        }
+return (dest);
+}

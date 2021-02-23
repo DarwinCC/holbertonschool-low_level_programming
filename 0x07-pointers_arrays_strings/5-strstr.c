e460637f89fb0d5d0f160e41ebde20befbbbd832
DarwinCC <darwinpkmz@gmail.com>
2021-02-23 04:09:44 +0000

    fix_2

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,15 +11,17 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
+			if (haystack[i + j] != needle[j])
 			{
-				return (&haystack[j]);
+			break;
 			}
 		}
+		if (needle[j] == 0)
+		return (&haystack[i++]);
 	}
 	return (NULL);
 }
